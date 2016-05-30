9284eb9ac270acc9aed01eec84768a37e7cca90e
Tfuqua <tfuqua@gwu.edu>
2016-05-30 14:04:01 -0400

    Added Single photo view and added commenting feature

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -30,7 +30,7 @@ const Photo = React.createClass({
               </button>
               <Link className="button" to={`/view/${post.code}`}>
                 <span className="comment-count">
-                  <span className="speech-bubble"></span>
+                  <span className="speech-bubble"></span>&nbsp;
                   {comments[post.code] ?
                     comments[post.code].length : 0 }
                 </span>
